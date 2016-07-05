93bef01030e5e5bca856ccbd8006c3f1632a22db
josephquested <josephquested@gmail.com>
2016-07-06 09:49:47 +1200

    add drone

---
+++ Assets/Scripts/Drone.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drone : MonoBehaviour
+{
+	NavMeshAgent agent;
+	Transform goal;
+
+	void Awake ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		goal = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void FixedUpdate ()
+	{
+		if (agent != null)
+		{
+			UpdateAgent();
+		}
+	}
+
+	void UpdateAgent ()
+	{
+		if (goal != null)
+		{
+			agent.destination = goal.position;
+		}
+		else
+		{
+			agent.destination = Vector3.zero;
+		}
+	}
+
+	void OnCollisionStay (Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			// collision.gameObject.BroadcastMessage("Damage", 0.1f);
+		}
+	}
+}
