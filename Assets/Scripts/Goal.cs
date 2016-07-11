213e9021992b2c6cacdf8c06d3086ba2b3f5d0b3
josephquested <josephquested@gmail.com>
2016-07-12 10:12:23 +1200

    add locked goal

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,48 +5,41 @@ using System.Collections.Generic;
 public class Goal : MonoBehaviour
 {
 	GameManager gameManager;
-	public List<GameObject> orbIcons = new List<GameObject>();
+	List<GameObject> orbIcons = new List<GameObject>();
 
+	public bool locked;
 	public GameObject orbIconPrefab;
-	public float delay;
+	public GameObject lockIcon;
+	public GameObject iconContainer;
 
 	void Awake ()
 	{
 		gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
 		StartCoroutine(CreateOrbIcons());
+		UpdateLock();
 	}
 
 	void Update ()
 	{
-		Rotate();
+		RotateIcons();
 	}
 
-	void Rotate ()
+	void UpdateLock ()
 	{
-		transform.Rotate(Vector3.up, Time.deltaTime * 100, Space.World);
-	}
-
-	IEnumerator CreateOrbIcons ()
-	{
-		GameObject[] orbs = GameObject.FindGameObjectsWithTag("Orb");
-
-		for (int i = 0; i < orbs.Length; i++)
+		if (orbIcons.Count == 0)
 		{
-			Vector3 position = new Vector3(
-				transform.position.x - 1f,
-				transform.position.y + 0.2f,
-				transform.position.z
-			);
+			locked = false;
+			Destroy(lockIcon);
+		}
+	}
 
-			GameObject icon = (GameObject)Instantiate(
-			orbIconPrefab,
-			position,
-			orbIconPrefab.transform.rotation
-			);
-
-			icon.transform.parent = transform;
-			orbIcons.Add(icon);
-			yield return new WaitForSeconds(delay);
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.CompareTag("Player") && !locked)
+		{
+			collider.GetComponent<Player>().active = false;
+			collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			gameManager.Win();
 		}
 	}
 
@@ -54,15 +47,31 @@ public class Goal : MonoBehaviour
 	{
 		Destroy(orbIcons[0]);
 		orbIcons.RemoveAt(0);
+		UpdateLock();
 	}
 
-	void OnTriggerEnter (Collider collider)
+	IEnumerator CreateOrbIcons ()
 	{
-		if (collider.CompareTag("Player"))
+		GameObject[] orbs = GameObject.FindGameObjectsWithTag("Orb");
+		for (int i = 0; i < orbs.Length; i++)
 		{
-			collider.GetComponent<Player>().active = false;
-			collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
-			gameManager.Win();
+			GameObject icon = (GameObject)Instantiate(
+				orbIconPrefab,
+				new Vector3(
+					transform.position.x - 1f,
+					transform.position.y + 0.2f,
+					transform.position.z),
+				orbIconPrefab.transform.rotation
+			);
+
+			icon.transform.parent = iconContainer.transform;
+			orbIcons.Add(icon);
+			yield return new WaitForSeconds(0.3f);
 		}
+	}
+
+	void RotateIcons ()
+	{
+		iconContainer.transform.Rotate(Vector3.up, Time.deltaTime * 100, Space.World);
 	}
 }
