fd080dfab23a0c76a1afa4e3bae12b6d04d5deb5
josephquested <josephquested@gmail.com>
2016-07-13 19:51:16 +1200

    add scene 3

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawn : MonoBehaviour
+{
+	public GameObject playerPrefab;
+
+	void Awake ()
+	{
+		Instantiate(playerPrefab,
+		transform.position - Vector3.down * -20,
+		transform.rotation);
+	}
+}
